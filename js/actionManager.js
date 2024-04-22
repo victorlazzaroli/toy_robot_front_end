3cb0afcd0bca68305af158b61e60d8b3279c5046
Victor <the_wizard_ita@hotmail.it>
2024-04-22 21:35:42 +0200

    Action Manager singleton

--- js/actionManager.js
+++ js/actionManager.js
@@ -1,4 +1,4 @@
-class ActionManager {
+export class ActionManager {
     static actionManager = new this();
     events = ['forward', 'backward', 'clockwise', 'counterclockwise']
     listeners = new Map();
@@ -11,21 +11,31 @@ class ActionManager {
         for (let eventType of this.events) {
             this.listeners.set(eventType, []);
         }
-        document.addEventListener('keydown', (event) => console.log(event));
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
+                case 'ArrowDown':
+                    this.listeners.get('backward').forEach(listener => listener('backward'));
+                    break;
+                case 'ArrowUp':
+                    this.listeners.get('forward').forEach(listener => listener('forward'));
+                    break;
+                case 'ArrowLeft':
+                    this.listeners.get('counterclockwise').forEach(listener => listener('counterclockwise'));
+                    break;
+                case 'ArrowRight':
+                    this.listeners.get('clockwise').forEach(listener => listener('clockwise'));
+                    break;
+            }
+        });
 
     }
 
     // Registra dei listener da avvisare qualora un evento occorre
     register(event, clbk) {
-
-    }
-
-    unregister (event, clbk) {
-
-    }
-
-    clearEventListeners (event) {
-
+        if ( !this.listeners.has(event) ) {
+            throw new Error('Invalid event');
+        }
+        this.listeners.get(event).push(clbk);
     }
 }
 
